bcbb7c7c3562c1e620e886e3f017fe3c698bed7a
vgusain <vgusain@VGUSAIN-IN.al.oracle.com>
2019-11-03 00:36:57 +0530

    config keys added

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,6 +1,5 @@
 module.exports = {
-    MongoURI : 'mongodb+srv://varun:root@socialmediaapp-veyho.mongodb.net/test?retryWrites=true&w=majority',
-    GoogleClientId: '287707906862-gk8ghjvodvda6jnhe29ej6mvb0q2me9s.apps.googleusercontent.com',
-    GoogleClientSecret: 'TTn7he0IHHw017h_9JxNgE0J'
-
+    MongoURI : process.env.MONGO_URI,
+    GoogleClientId: process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
